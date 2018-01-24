00eb0652d8c8a0443442116b1d0243c4972bfc04
echo 'hello world <ssmyth1071@gmail.com>
2018-01-23 20:51:50 -0500

    added todo_items

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,33 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    if @todo_item.save
+      redirect_to @todo_list
+    else
+      flash[:error] = "Todo list item could not be created"
+    end
+  end
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy 
+      flash[:success] = "To do list item was deleted"
+    else
+      flash[:error] = "Todo list item could not be deleted"
+    end
+    redirect_to @todo_list
+  end
+  private
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def todo_item_params
+      params.require(:todo_item).permit(:content)
+
+    end
+
+
+end
