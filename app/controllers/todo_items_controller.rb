7cc91bbb7bd81feb90697fd937897daca8d5249c
echo 'hello world <ssmyth1071@gmail.com>
2018-01-25 09:33:07 -0500

    finish styling

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,7 +4,7 @@ class TodoItemsController < ApplicationController
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     if @todo_item.save
-      redirect_to @todo_list
+      redirect_to @todo_list, notice: "Todo item created successfully."
     else
       flash[:error] = "Todo list item could not be created"
     end
